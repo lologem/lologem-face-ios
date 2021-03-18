c868adf8d47c34c0bd8bec2408ce3515c234ae61
kjkim <>
2021-03-18 22:04:19 +0900

    initial commit

---
+++ LologemFace.xcframework/ios-arm64_x86_64-simulator/LologemFace.framework/Headers/LologemFace.h
@@ -0,0 +1,18 @@
+//
+//  LologemFace.h
+//  LologemFace
+//
+//  Created by mchello on 2021/02/03.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LologemFace.
+FOUNDATION_EXPORT double LologemFaceVersionNumber;
+
+//! Project version string for LologemFace.
+FOUNDATION_EXPORT const unsigned char LologemFaceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LologemFace/PublicHeader.h>
+
+
